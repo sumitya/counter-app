031909700684b42cea410fab3f5c0c2f58e3cab6
syadav <syadavgit@gmail.com>
2021-12-26 17:32:10 +0530

    display image in the component

---
+++ src/components/counter.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+
+    state = {
+        count:0,
+        imageUrl: 'https://picsum.photos/200'
+    };
+   
+    render() { 
+    
+        return (
+            <div>
+            <img src={this.state.imageUrl} alt=""></img>
+            <span>{this.formatCount()}</span>
+            <button>Increment</button>
+            </div>
+        )
+    }
+
+    formatCount(){
+        const {count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+ 
+export default Counter;
