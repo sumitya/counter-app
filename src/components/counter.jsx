07ee7511a46da2699b928ee27f78d41d0a160241
syadav <syadavgit@gmail.com>
2021-12-26 17:44:00 +0530

    apply styles within JSX elements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,16 +4,20 @@ class Counter extends React.Component {
 
     state = {
         count:0,
-        imageUrl: 'https://picsum.photos/200'
+      
+    };
+
+    styles = {
+        fontSize:50,
+        fontWeigt:"bold"
     };
    
     render() { 
     
         return (
             <div>
-            <img src={this.state.imageUrl} alt=""></img>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         )
     }
