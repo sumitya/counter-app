c3672ae64d75d9180c0f5171c0744765aaffed3d
syadav <syadavgit@gmail.com>
2021-12-26 18:55:55 +0530

    conditional rendering of elements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,12 +4,12 @@ class Counter extends React.Component {
 
     state = {
         count:0,
-        tags:['tag1','tag2','tag3']
+        tags:[]
     };
 
     styles = {
         fontSize:50,
-        fontWeigt:"bold"
+        fontWeight:"bold"
     };
    
     render() {
@@ -18,11 +18,16 @@ class Counter extends React.Component {
             <div>
             <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map( tag => <li key={tag}>{tag}</li>)}
-            </ul>
+            
+               {this.state.tags.length===0 && "Please create a new tag!"}
+               {this.renderTags()}
+           
             </div>
         )
+    }
+
+    renderTags() {
+       return <ul>{this.state.tags.map( tag => <li key={tag}>{tag}</li>)}</ul>
     }
 
     getBadgeClasses() {
