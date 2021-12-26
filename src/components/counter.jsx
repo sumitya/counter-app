222e8c58b2d8bc43a4bde16ba1d3976cee012e5d
syadav <syadavgit@gmail.com>
2021-12-26 18:35:50 +0530

    moving variables to a function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,7 @@ class Counter extends React.Component {
 
     state = {
         count:0,
-      
+        tags:['tag1','tag2','tag3']
     };
 
     styles = {
@@ -12,16 +12,23 @@ class Counter extends React.Component {
         fontWeigt:"bold"
     };
    
-    render() { 
-        let classes = "badge m-2 badge-"
-        classes += this.state.count ===0 ? "warning" : "primary";
+    render() {
     
         return (
             <div>
-            <span style={this.styles} className={classes}>{this.formatCount()}</span>
+            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {this.state.tags.map( tag => <li key={tag}>{tag}</li>)}
+            </ul>
             </div>
         )
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
